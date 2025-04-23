d78a2d763c4cca7e6f1ed43dfbf4cb25109dd506
Anyel3128 <anyeldelcarmen88@gmail.com>
2025-04-22 20:53:54 -0500

    hola

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,65 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { Link } from 'expo-router';
 
-export default function App() {
+const index = () => {
   return (
     <View style={styles.container}>
-      <Text>¡Hola, Alejandra! ✨ Tu app está lista.</Text>
+      <Image source={require('../imagenes/logo.jpeg')} style={styles.image} />
+
+      <Text style={styles.pinkText}>¡Bienvenido al Gestor de gastos!</Text>
+      <Text style={styles.pinkclaroText}>
+        Gestor de gastos es una aplicación para controlar los gastos de una manera rápida y sencilla
+      </Text>
+
+      <Link href="/pantalla2" asChild>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Comenzar</Text>
+        </TouchableOpacity>
+      </Link>
     </View>
   );
-}
+};
+
+export default index;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#fff',
+    width: '100%',
+    height: '100%',
+  },
+  image: {
+    width: 120,
+    height: 120,
+    marginBottom: 20,
+    resizeMode: 'contain',
+  },
+  pinkText: {
+    color: '#FF007F',
+    fontSize: 20,
+    marginBottom: 10,
+  },
+  pinkclaroText: {
+    color: '#FFB6C1',
+    fontSize: 20,
+    marginBottom: 10,
+    textAlign: 'center',
+    paddingHorizontal: 20,
+  },
+  button: {
+    backgroundColor: '#FFB6C1',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    marginTop: 20,
+  },
+  buttonText: {
+    color: '#FF007F',
+    fontSize: 18,
+    textAlign: 'center',
   },
 });
---
+++ app/pantalla2.tsx
@@ -0,0 +1,216 @@
+import React, { useState } from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  Modal,
+  TextInput,
+  TouchableOpacity,
+  ImageBackground,
+} from 'react-native';
+
+const Pantalla2 = () => {
+  const [presupuesto, setPresupuesto] = useState('');
+  const [presupuestoRestante, setPresupuestoRestante] = useState('');
+  const [mostrarPresupuestoModal, setMostrarPresupuestoModal] = useState(true);
+  const [mostrarGastoModal, setMostrarGastoModal] = useState(false);
+  const [inputPresupuesto, setInputPresupuesto] = useState('');
+  const [inputGasto, setInputGasto] = useState('');
+  const [verArticulo, setVerArticulo] = useState(false);
+
+  const confirmarPresupuesto = () => {
+    setPresupuesto(inputPresupuesto);
+    setPresupuestoRestante(inputPresupuesto);
+    setMostrarPresupuestoModal(false);
+  };
+
+  const registrarGasto = () => {
+    const gasto = parseFloat(inputGasto);
+    const restante = parseFloat(presupuestoRestante);
+    if (!isNaN(gasto) && gasto <= restante) {
+      const nuevoRestante = restante - gasto;
+      setPresupuestoRestante(nuevoRestante.toFixed(2));
+      setMostrarGastoModal(false);
+      setInputGasto('');
+    } else {
+      alert('Gasto inválido o mayor al presupuesto restante.');
+    }
+  };
+
+  return (
+    <ImageBackground
+      source={require('../imagenes/fondo2.jpeg')}
+      style={styles.background}
+      resizeMode="cover"
+    >
+      <View style={styles.overlay}>
+        {/* ventana Modal para presupuesto */}
+        <Modal visible={mostrarPresupuestoModal} transparent animationType="slide">
+          <View style={styles.modalContainer}>
+            <View style={styles.modalContent}>
+              <Text style={styles.label}>¿Cuál es tu presupuesto semanal?</Text>
+              <TextInput
+                style={styles.input}
+                keyboardType="numeric"
+                value={inputPresupuesto}
+                onChangeText={setInputPresupuesto}
+                placeholder="Ej: 100.00"
+              />
+              <TouchableOpacity onPress={confirmarPresupuesto} style={styles.botonGasto}>
+                <Text style={styles.botonTexto}>Confirmar</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </Modal>
+
+        {/* ventana Modal para gasto */}
+        <Modal visible={mostrarGastoModal} transparent animationType="slide">
+          <View style={styles.modalContainer}>
+            <View style={styles.modalContent}>
+              <Text style={styles.label}>¿Cuánto has gastado?</Text>
+              <TextInput
+                style={styles.input}
+                keyboardType="numeric"
+                value={inputGasto}
+                onChangeText={setInputGasto}
+                placeholder="Ej: 20.00"
+              />
+              <TouchableOpacity onPress={registrarGasto} style={styles.botonGasto}>
+                <Text style={styles.botonTexto}>Registrar gasto</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </Modal>
+
+        {/* lo que aparece en la pagina */}
+        {presupuesto !== '' && (
+          <>
+            <Text style={styles.tituloSuperior}>Control de tu presupuesto</Text>
+            <Text style={styles.presupuestoTexto}>${presupuestoRestante}</Text>
+            <Text style={styles.textoInfo}>
+              Este es tu presupuesto semanal. Abajo, agrega tus gastos.
+            </Text>
+
+            <TouchableOpacity
+              style={styles.botonGasto}
+              onPress={() => setMostrarGastoModal(true)}
+            >
+              <Text style={styles.botonTexto}>Registrar gasto</Text>
+            </TouchableOpacity>
+
+            <Text style={styles.textoInfo}>¿Quieres ver el aporte del artículo 177?</Text>
+            <TouchableOpacity
+              style={styles.botonVer}
+              onPress={() => setVerArticulo(!verArticulo)}
+            >
+              <Text style={styles.botonTexto}>Ver</Text>
+            </TouchableOpacity>
+
+            {verArticulo && (
+              <Text style={styles.articuloTexto}>
+                El artículo 177 establece que el Beneficio Definido del Componente
+                 de Viudez será equivalente al 50% de la pensión de vejez o invalidez
+                  que recibía o le habría correspondido al causante al momento de fallecer. 
+                  Esta pensión se pagará por un período de cinco años desde la fecha del fallecimiento, 
+                  salvo que la viuda o el viudo esté en condición de invalidez o tenga hijos del causante 
+                  con derecho a pensión, en cuyo caso se seguirá pagando mientras se mantengan esas condiciones.
+                   Por su parte, el artículo 181 señala que en el Subsistema Mixto, el monto personal del Beneficio
+                    Definido correspondiente a la pensión de viudez será una 
+                suma fija de quinientos balboas (B/. 500.00), siempre que el asegurado participara en dicho componente.
+              </Text>
+            )}
+          </>
+        )}
+      </View>
+    </ImageBackground>
+  );
+};
+
+export default Pantalla2;
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+  },
+  overlay: {
+    flex: 1,
+    backgroundColor: '#ffffffcc', // Fondo blanco con transparencia
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  tituloSuperior: {
+    fontSize: 35,
+    fontWeight: 'bold',
+    color: '#FF007F',
+    marginBottom: 10,
+    textAlign: 'center',
+
+  },
+  presupuestoTexto: {
+    fontSize: 67,
+    fontWeight: 'bold',
+    color: '#FF007F',
+    marginBottom: 10,
+    marginTop: 50,
+
+  },
+  textoInfo: {
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#555',
+    marginBottom: 10,
+  },
+  botonGasto: {
+    backgroundColor: '#FFB6C1',
+    paddingVertical: 12,
+    paddingHorizontal: 30,
+    borderRadius: 8,
+    marginBottom: 70,
+  },
+  botonVer: {
+    backgroundColor: '#FFD1DC',
+    paddingVertical: 10,
+    paddingHorizontal: 25,
+    borderRadius: 8,
+    marginBottom: 10,
+  },
+  botonTexto: {
+    color: '#FF007F',
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  articuloTexto: {
+    fontSize: 16,
+    color: '#333',
+    textAlign: 'center',
+    marginTop: 10,
+    paddingHorizontal: 10,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#00000088',
+    paddingHorizontal: 20,
+  },
+  modalContent: {
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    padding: 25,
+    elevation: 5,
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 10,
+    textAlign: 'center',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#FFB6C1',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 15,
+    fontSize: 18,
+  },
+});
